775602e16e5dbd43e76ce78ef12a3a7a9693dddd
root <root@ola-UX32A.(none)>
2014-02-10 18:52:48 +0100

    add photo

--- sidor/profil.php
+++ sidor/profil.php
@@ -42,8 +42,46 @@ if($cmd == 'passwd') {
 					"city = '".$_POST['city']."' WHERE id = '".$_SESSION['userID']."';") or die(mysql_error());
 		echo 'Profil sparad!<br>'.
 				'<input type=button class=btn value="Tillbaka" onClick="document.location=\'index.php?page=default\';">';
-					
-		
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 200000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "pic/users/".$_SESSION['foto'] . $_FILES["file"]["name"]);
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+
+
 	} else {
 		// STANDARD
 		$dbuser = mysql_fetch_array(mysql_query("SELECT * FROM users WHERE id = '".$_SESSION['userID']."';"), MYSQL_ASSOC);
@@ -87,6 +125,7 @@ if($cmd == 'passwd') {
 						<input type="hidden" name="register" value="register">
 						<td></td>
 						<td align="left"><input type="button" value="Tillbaka" onClick="history.back();" class="btn"></td>
+						<input type="file" name="file" id="file"><br>
 						<td align="right"><input type="button" value="Spara profil" onClick="this.form.action='index.php?sida=profil'; this.form.submit();" class="btn"></td>
 					</tr>
 				</table>
